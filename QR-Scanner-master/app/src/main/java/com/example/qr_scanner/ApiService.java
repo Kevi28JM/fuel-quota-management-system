65f5be07db79bc0f2f13c198d9ae6d3a13eacc5c
TharuHiru <hirusha-im21011@stu.kln.ac.lk>
2025-05-23 00:07:46 +0530

    updated services to connetc the emulator

--- QR-Scanner-master/app/src/main/java/com/example/qr_scanner/ApiService.java
+++ QR-Scanner-master/app/src/main/java/com/example/qr_scanner/ApiService.java
@@ -5,6 +5,6 @@ import retrofit2.http.Body;
 import retrofit2.http.POST;
 
 public interface ApiService {
-    @POST("/register") // This is the endpoint from your Node.js API
+    @POST("/stationOperator") // This is the endpoint from your Node.js API
     Call<Void> sendRegistration(@Body PendingRequest pendingRequest);
 }
