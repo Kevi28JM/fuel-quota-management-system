d78f7859257cf03b600e7ebfb094063b96ad4747
TharuHiru <hirusha-im21011@stu.kln.ac.lk>
2025-05-23 15:33:47 +0530

    updated app to fetch stations available

--- QR-Scanner-master/app/src/main/java/com/example/qr_scanner/ApiService.java
+++ QR-Scanner-master/app/src/main/java/com/example/qr_scanner/ApiService.java
@@ -1,14 +1,19 @@
 package com.example.qr_scanner;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 public interface ApiService {
     @POST("/stationOperator") // This is the endpoint from your Node.js API
     Call<ApiResponse> sendRegistration(@Body PendingRequest pendingRequest);
 
-
     @POST("/stationOperator/login")
     Call<LoginResponse> loginStationOperator(@Body LoginRequest loginRequest);
+
+    @GET("stationOperator/stations") // Changed to match your backend
+    Call<List<Station>> getStations();
 }
