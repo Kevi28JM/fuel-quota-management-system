56be3ade47245ddb57272a4ce301912fcc4a58e5
haisndu <thirasa-im21062@stu.kln.ac.lk>
2025-05-23 20:26:37 +0530

    update #2

--- QR-Scanner-master/app/src/main/java/com/example/qr_scanner/RetrofitClient.java
+++ QR-Scanner-master/app/src/main/java/com/example/qr_scanner/RetrofitClient.java
@@ -7,7 +7,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static final String BASE_URL = "http://192.168.8.101:5000/"; // Add ur wifi IP for usb emulator
+    private static final String BASE_URL = "http://192.168.8.158:5000/"; // Add ur wifi IP for usb emulator
     private static Retrofit retrofit;
 
     public static Retrofit getRetrofitInstance() {
